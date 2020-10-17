646bf489b7ea68793af89f79e97ef3f108cd0e30
Riki <sawariki@gmail.com>
2020-10-17 12:57:29 +0300

    Добавьте файлы проекта.

---
+++ DZ_07_SergSavk/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace DZ_07_SergSavk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.btnCommand0 = new System.Windows.Forms.Button();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(652, 107);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "button1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(652, 178);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "button2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(652, 267);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "button3";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(122, 178);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "label1";
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Location = new System.Drawing.Point(125, 233);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber1.TabIndex = 4;
+            this.lblNumber1.Text = "label1";
+            // 
+            // btnCommand0
+            // 
+            this.btnCommand0.Location = new System.Drawing.Point(652, 44);
+            this.btnCommand0.Name = "btnCommand0";
+            this.btnCommand0.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand0.TabIndex = 5;
+            this.btnCommand0.Text = "button0";
+            this.btnCommand0.UseVisualStyleBackColor = true;
+            this.btnCommand0.Click += new System.EventHandler(this.btnCommand0_Click);
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Location = new System.Drawing.Point(336, 346);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber2.TabIndex = 6;
+            this.lblNumber2.Text = "label1";
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(652, 233);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 7;
+            this.btnLast.Text = "button4";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.btnCommand0);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Button btnCommand0;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.Button btnLast;
+    }
+}
+
---
+++ DZ_07_SergSavk/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ_07_SergSavk
+{
+    public partial class Form1 : Form
+    {
+        public int value;
+        public bool flagPlay = false;
+        //public int move = 0;
+        public string last_Move = "0";
+
+        public void Win()
+        {
+            if (int.Parse(lblNumber.Text) == value)
+            {
+                lblNumber2.Text = "Вы выйграли за " + lblNumber1.Text + " ходов," + " набрав число " + value;
+                lblNumber1.Text = "0";
+                flagPlay = false;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnCommand0.Text = "Play";
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "x2";
+            btnReset.Text = "Сброс";
+            btnLast.Text = "-1 ход";
+            lblNumber.Text = "0";
+            lblNumber1.Text = "0";
+            lblNumber2.Text = "0";            
+            this.Text = "Удвоитель";
+            
+        }
+        public void btnCommand0_Click(object sender, EventArgs e)
+        {
+            //value = 5;
+            Random r = new Random();
+            value = r.Next(10, 110);           
+            lblNumber2.Text = "Наберите заданное число " + value.ToString();
+            flagPlay = true;
+            lblNumber.Text = "0";
+            lblNumber1.Text = "0";
+        }
+
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            //move = 1;
+            last_Move = lblNumber.Text;
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            lblNumber1.Text = (int.Parse(lblNumber1.Text) + 1).ToString();
+            Win();
+            
+        }
+
+
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            //move = 2;
+            last_Move = lblNumber.Text;
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            lblNumber1.Text = (int.Parse(lblNumber1.Text) + 1).ToString();
+            Win();
+            
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            if (flagPlay == false)
+            {
+                lblNumber.Text = "0";
+                lblNumber1.Text = "0";
+            }
+            else
+            {
+                lblNumber.Text = "0";
+                lblNumber1.Text = (int.Parse(lblNumber1.Text) + 1).ToString();
+            }            
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            
+            lblNumber.Text = last_Move;
+        }
+    }
+}
